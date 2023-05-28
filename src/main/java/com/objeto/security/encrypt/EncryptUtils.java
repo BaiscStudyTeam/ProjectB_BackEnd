0e6fb3dee77142f981114a449acacc13c90a698b
yoosonglee <zaxscd95@naver.com>
2023-05-28 12:03:32 +0900

    [SRC] UPDATE EncryptUtils comment

--- src/main/java/com/objeto/security/encrypt/EncryptUtils.java
+++ src/main/java/com/objeto/security/encrypt/EncryptUtils.java
@@ -19,7 +19,8 @@ public class EncryptUtils {
      * @return password encoded
      */
     public static String executeBCryptPasswordEncoder(String charseq) {
-        // 버그픽스된 최신 BCrypt 알고리즘과 max 수치의  로그라운드 횟수로 Encoder 생성
+        // 버그픽스된 최신 BCrypt 알고리즘
+        // max 수치의 로그라운드 ( 31) 로 생성시 암호화에 엄청난 시간이 걸리므로 로그라운드 수치는 기본 제공되는 default (10) 을 사용
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(
                 BCryptPasswordEncoder.BCryptVersion.$2Y);
         return encoder.encode(charseq);
