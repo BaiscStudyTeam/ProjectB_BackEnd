b225635c28cbcf9331327a89ecbb93603bcd708d
yoosonglee <zaxscd95@naver.com>
2023-08-19 01:25:57 +0900

    [SRC] ADD jakarta Validation Check MethodArgumentNotValidException

--- src/main/java/com/objeto/common/GlobalExceptionHandler.java
+++ src/main/java/com/objeto/common/GlobalExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.objeto.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,5 +24,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<String> handleJwtAuthException(Exception e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Jwt Auth Failed");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(Exception e) throws JsonProcessingException {
+        MethodArgumentNotValidException exception = (MethodArgumentNotValidException) e;
+        List<ObjectError> list = exception.getBindingResult().getAllErrors();
+        ObjectMapper mapper = new ObjectMapper();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mapper.writeValueAsString(list));
     }
 }
