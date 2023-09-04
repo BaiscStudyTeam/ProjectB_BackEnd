9ec6ebde84572b3ad53058a344077116ec1ff01f
yoosonglee <zaxscd95@naver.com>
2023-09-04 20:56:11 +0900

    [CFG] Change Allow Origin value to config file to enforce security

--- src/main/java/com/objeto/security/WebMvcConfigTest.java
+++ src/main/java/com/objeto/security/WebMvcConfigTest.java
@@ -1,5 +1,6 @@
 package com.objeto.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfigTest implements WebMvcConfigurer {
 
+
+    @Value("${spring.custom.allowed-origin}")
+    private String allowedOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.124.25.59")
+                .allowedOrigins("http://" + allowedOrigin)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
